3019dfc1f6768791bf653c871b91b963dace8ec2
2755802910@qq.com <2755802910@qq.com>
2025-11-21 15:17:16 +0800

    添加禁止部分店铺下单的功能； 后台可修改； gateway对下单信息再次校验，前端也会校验，不允许在封禁店铺下单 同时修改notifyUsers来源

---
+++ LuoliCommon/DTO/Admin/BannedBranchesRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuoliCommon.DTO.Admin
+{
+    public class BannedBranchesRequest
+    {
+
+        public string targetProxy { get; set; }
+            
+        public string[] branches { get; set; }
+    }
+}
