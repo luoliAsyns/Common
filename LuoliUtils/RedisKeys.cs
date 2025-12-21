318b326ab2df5d60a6d084da3852ec3c8afb6ceb
2755802910@qq.com <2755802910@qq.com>
2025-12-21 15:07:02 +0800

    25.12.21.1   修改缓存时间, 默认60秒，可从redis里配置 因为CI基本不变动

--- LuoliUtils/RedisKeys.cs
+++ LuoliUtils/RedisKeys.cs
@@ -6,39 +6,44 @@
         /// <summary>
         /// hashtable
         /// </summary>
-        public static string SkuId2Proxy = "skuid2proxy";
-        public static string AgisoAccessToken = "agiso_access_token";
+        public const string SkuId2Proxy = "skuid2proxy";
+        public const string AgisoAccessToken = "agiso_access_token";
 
 
         //用于去重
-        public static string ReceivedExternalOrder = "received.external.orders";
-        public static string NotUsedCoupons = "notused.coupons";
+        public const string ReceivedExternalOrder = "received.external.orders";
+        public const string NotUsedCoupons = "notused.coupons";
 
-        public static string PlaceOrderButtonEnable = "place.order.button.enable";//hset 多平台
+        public const string PlaceOrderButtonEnable = "place.order.button.enable";//hset 多平台
 
 
         //Sexytea相关
-        public static string SexyteaBranchId2City = "sexytea.branchid2city";
-        public static string SexyteaTokenAccount = "sexytea.tokens"; //hset 多账号
-        public static string SexyteaBannedBranchId = "sexytea.banned.branchids";
+        public const string SexyteaBranchId2City = "sexytea.branchid2city";
+        public const string SexyteaTokenAccount = "sexytea.tokens"; //hset 多账号
+        public const string SexyteaBannedBranchId = "sexytea.banned.branchids";
 
 
 
-        public static string NotifyUsers = "notify.users";
+        public const string NotifyUsers = "notify.users";
 
         //publish channels
-        public static string Pub_RefreshShipStatus="refresh.ship.status";
-        public static string Pub_RefreshPlaceOrderStatus = "refresh.placeorder.status";
+        public const string Pub_RefreshShipStatus="refresh.ship.status";
+        public const string Pub_RefreshPlaceOrderStatus = "refresh.placeorder.status";
 
         //for Prometheus
-        public static string Prom_ReceivedOrders = "prom.external-order.pull";
-        public static string Prom_ReceivedRefund = "prom.external-order.refund";
-        public static string Prom_CouponsGenerated = "prom.coupon.generated";
-        public static string Prom_Shipped = "prom.shipbot.shipped";
-        public static string Prom_ShipFailed = "prom.shipbot.failed";
-        public static string Prom_ReceivedConsumeInfo = "prom.consume-info.received";
-        public static string Prom_InsertedConsumeInfo = "prom.consume-info.inserted";
-        public static string Prom_PlacedOrders = "prom.placeorderbot.placed";
-        public static string Prom_PlacedOrdersFailed = "prom.placeorderbot.failed";
+        public const string Prom_ReceivedOrders = "prom.external-order.pull";
+        public const string Prom_ReceivedRefund = "prom.external-order.refund";
+        public const string Prom_CouponsGenerated = "prom.coupon.generated";
+        public const string Prom_Shipped = "prom.shipbot.shipped";
+        public const string Prom_ShipFailed = "prom.shipbot.failed";
+        public const string Prom_ReceivedConsumeInfo = "prom.consume-info.received";
+        public const string Prom_InsertedConsumeInfo = "prom.consume-info.inserted";
+        public const string Prom_PlacedOrders = "prom.placeorderbot.placed";
+        public const string Prom_PlacedOrdersFailed = "prom.placeorderbot.failed";
+
+        //for cache store sec
+        public const string CICacheStoreSec = "ci.cache.store.sec";
+        public const string EOCacheStoreSec = "eo.cache.store.sec";
+        public const string CouponCacheStoreSec = "coupon.cache.store.sec";
     }
 }
