7d9399237ccbf54fb53b4d4e479793808aed9e71
2755802910@qq.com <2755802910@qq.com>
2026-01-04 21:48:43 +0800

    refit设置放到Common中

---
+++ LuoliCommon/RefitSetting.cs
@@ -0,0 +1,32 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace LuoliCommon
+{
+    public static class RefitSetting
+    {
+
+        public static RefitSettings LuoliRefitSettings()
+        {
+            var jsonSerializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = false
+            };
+            var refitSetting = new RefitSettings()
+            {
+                ContentSerializer = new SystemTextJsonContentSerializer(jsonSerializerOptions)
+            };
+
+            return refitSetting;
+
+        }
+
+    }
+}
